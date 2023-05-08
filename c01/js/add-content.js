b58498b035218854094f79a2ed238cd20d077023
wawahuang <128460174+wawahuang@users.noreply.github.com>
2023-05-08 11:36:32 +0800

    modify

--- c01/js/add-content.js
+++ c01/js/add-content.js
@@ -1,5 +1,17 @@
 "use strict";
-var myname = "黃秀群";
-console.log(myname);
-var last_name = "chung";
-console.log(last_name);
+let today = new Date();
+let hourNow = today.getHours();
+let greeting;
+if (hourNow > 18) {
+    greeting = 'Good evening!';
+}
+else if (hourNow > 12) {
+    greeting = 'Good afternoon!';
+}
+else if (hourNow > 0) {
+    greeting = 'Good morning';
+}
+else {
+    greeting = 'Welcome!';
+}
+document.write('<h3>' + greeting + '</h3>');
--- c01/ts/add-content.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var myname = "黃秀群";
-console.log(myname);
-var last_name = "chung";
-console.log(last_name);

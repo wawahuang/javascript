e2de9cece8995fe97f2786ea37f2324bc4f6d412
wawahuang <128460174+wawahuang@users.noreply.github.com>
2023-05-08 11:18:10 +0800

    modify

---
+++ c01/ts/add-content.js
@@ -0,0 +1,5 @@
+"use strict";
+var myname = "黃秀群";
+console.log(myname);
+var last_name = "chung";
+console.log(last_name);
---
+++ c01/ts/add-content.ts
@@ -0,0 +1,4 @@
+var myname = "黃秀群";
+console.log(myname);
+var last_name = "chung";
+console.log(last_name);

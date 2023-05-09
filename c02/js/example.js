58132d5bd4d7a515a5a39900566409992fe8ee39
wawahuang <128460174+wawahuang@users.noreply.github.com>
2023-05-09 11:15:35 +0800

    modify

---
+++ c02/js/example.js
@@ -0,0 +1,6 @@
+let greeting ="Huang";
+let my_name = "Xiu Chun"
+let message = ',please check you order:';
+let welcome = greeting +my_name +message;
+let greetingElement =document.getElementById('greeting');
+greetingElement.innerText=welcome;

1aa6175288a34db0a452f7ba2d65e749e68634fa
wawahuang <128460174+wawahuang@users.noreply.github.com>
2023-05-09 14:02:47 +0800

    modify

--- c03/js/window-object.js
+++ c03/js/window-object.js
@@ -1,6 +1,6 @@
 let msg = '<h2>browser window</h2>'
-msg +=`<p>browser_width:${window.innerWidth}</p>`
-msg +=`<p>browser_height:${window.innerHeight}</P>`
+msg += `<p>browser_width:${window.innerWidth}</p>`
+msg += `<p>browser_height:${window.innerHeight}</P>`
 
 info_element= document.getElementById('info')
 info_element.innerHTML = msg;

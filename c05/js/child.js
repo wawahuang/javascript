dd6917c21d6875bd27f7c3bf3bf992f5e1342faf
wawahuang <128460174+wawahuang@users.noreply.github.com>
2023-05-15 08:38:21 +0800

    modify

---
+++ c05/js/child.js
@@ -0,0 +1,10 @@
+let ul_element = document.getElementsByTagName('ul')[0];
+console.log(ul_element.length)
+
+ul_element.firstElementChild.className = 'complete'
+ul_element.lastElementChild.className = 'cool'
+/*let first_child = ul_element.firstElementChild;
+let last_child = ul_element.lastElementChild;
+
+first_child.className = 'complete'
+last_child.className = 'cool';*/

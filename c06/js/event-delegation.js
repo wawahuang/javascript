7e4421ccf82a3a5614dbc3c9334be6bada6a98f7
wawahuang <128460174+wawahuang@users.noreply.github.com>
2023-05-15 15:50:16 +0800

    modify

---
+++ c06/js/event-delegation.js
@@ -0,0 +1,8 @@
+document.getElementById('shoppingList').addEventListener('click',function(event){
+    event.preventDefault();
+    let liElement = event.target.parentNode;
+
+    //let ulElement = event.currentTarget;
+    //ulElement.removeChild(liElement);
+    liElement.remove();
+})

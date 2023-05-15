068e5b9b3612e8678fb0c1125dc2f9494cfa9a3a
wawahuang <128460174+wawahuang@users.noreply.github.com>
2023-05-15 10:25:15 +0800

    modify

--- c05/js/inner-text-and-text-content.js
+++ c05/js/inner-text-and-text-content.js
@@ -1,5 +1,5 @@
 let ElementOne = document.getElementById('one')
-//let innerHTML_string = ElementOne.innerHTML
+let innerHTML_string = ElementOne.innerHTML
 let innerText_string = ElementOne.innerText
 let textContent_string = ElementOne.textContent
 

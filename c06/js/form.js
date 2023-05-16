1003764a254e11c26456b089a6fbcfa85e5445ba
wawahuang <128460174+wawahuang@users.noreply.github.com>
2023-05-16 10:16:56 +0800

    modify

--- c06/js/form.js
+++ c06/js/form.js
@@ -1,5 +1,8 @@
 let formElement = document.getElementById('formSignup')
 let packageElement = document.getElementById('package')
+let packageHintElement = document.getElementById('packageHint')
+let termsElement = document.getElementById('terms')
+let termsHintElement = document.getElementById('termsHint')
 
 formElement.addEventListener('submit',(event)=>{
     console.log('submitting')
--- c06/js/keypress.js
+++ c06/js/keypress.js
@@ -1,9 +1,10 @@
 let messageElement = document.getElementById('message');
-let lastkeyElement = document.getElementById('lastkey');
-let charDisplay = document.getElementById('charDisplay');
-messageElement.addEventListener('keyup',(event)=>{
+let lastkeyElement = document.getElementById('lastKey');
+let charDisplay = document.getElementById('charactersLeft');
+
+messageElement.addEventListener('keyup', (event) => {
     lastkeyElement.textContent = `最後一個key:${event.key},keycode:${event.code}`;
-    let textEnter = messageElement.value
+    let textEntered = messageElement.value
     let counter = 180-textEntered.length
     charDisplay.textContent = `還剩下:${counter}個字`
 });

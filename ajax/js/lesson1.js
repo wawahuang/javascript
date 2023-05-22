5e895789e39bba7ea5d83aee1f1252d0fd73a436
wawahuang <128460174+wawahuang@users.noreply.github.com>
2023-05-22 13:43:27 +0800

    modify

--- ajax/js/lesson1.js
+++ ajax/js/lesson1.js
@@ -1,18 +1,47 @@
 let sarea_array = []
 let sareaElement =  document.getElementById('sarea');
 let areaNameElement = document.getElementById('areaName')
+let tbodyElement = document.getElementById('tbody')
+let youbikedata;
+
 sareaElement.addEventListener('change', (event) => {
     let selectedIndex = sareaElement.selectedIndex;
     selectedValue=sareaElement.options[selectedIndex].value
     if(sarea_array.includes(selectedValue)){
-        console.log(`行政區:${selectedValue}`)
+        //console.log(`行政區:${selectedValue}`)
         areaNameElement.innerText = selectedValue
-        
+        let trHTML = "" //行政區內的html資訊
+        youbikedata.forEach(element => {            
+            if (element.sarea == selectedValue){
+                if (element.act == "1"){
+                    var status = "營業中"
+                }else{
+                    var status = "維護中"
+                }
+
+                trHTML += "<tr>"
+                trHTML += "<td>" + element.sna.slice(11) +"</td>"
+                trHTML += "<td>" + element.ar +"</td>"
+                trHTML += "<td>" + element.tot +"</td>"
+                trHTML += "<td>" + element.sbi +"</td>"
+                trHTML += "<td>" + element.bemp +"</td>"
+                trHTML += "<td>" + element.updateTime + "</td>"
+                trHTML += "<td>" + status +"</td>"
+                trHTML += "</tr>"
+            }
+
+        }); 
+        tbodyElement.innerHTML = trHTML
     }
 });
 
 function reqListener() {
-    let youbikedata = JSON.parse(this.responseText)    
+    console.log(this.status)
+    if(this.status != 200){
+        console.log("網頁維護中...")
+        return 
+    }
+    youbikedata = JSON.parse(this.responseText)    
     for(const youbike of youbikedata){
         sarea_array.push(youbike.sarea)    
     }
@@ -28,11 +57,17 @@ function reqListener() {
     }
 }
 
+function reqError(){
+    console.log("網頁維護中")
+}
+
 const windowload = (event) => {
     console.log('網頁已經全部被載入');    
     const req = new XMLHttpRequest();
     req.addEventListener("load", reqListener);
-    req.open("GET", "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json");
+    req.addEventListener("error",reqError)
+    //req.open("GET", "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json");
+    req.open("GET", "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v1/youbike_immediate.json");
     req.send();
 }
 

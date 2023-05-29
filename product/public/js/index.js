f88182ac638727a37ab6695bc0701c48f24e2e20
wawahuang <128460174+wawahuang@users.noreply.github.com>
2023-05-29 10:10:07 +0800

    modify

--- product/public/js/index.js
+++ product/public/js/index.js
@@ -1,12 +1,60 @@
 let formElement = document.querySelector('#form')
-let inputNameElement = document.querySelector('#inputName')
 
-const validateName = () =>{
+let allOfDataArray = [];
+
+const validateName = ()=>{
+    let inputNameElement = document.querySelector('#inputName')
+    let nameAlertElement = document.querySelector('#nameAlert')
     let productName = inputNameElement.value
-    console.log(productName)
+    if (productName.length == 0){
+        //console.log("沒有填資料")
+        nameAlertElement.classList.remove("close")
+        return
+    }
+    allOfDataArray.push({'productName':productName})
+}
+
+const validateCodeFormat = () =>{
+    let inputCodeElement = document.querySelector('#inputCode')
+    let codeAlertElement = document.querySelector('#codeAlert')
+    let inputCodeValue = inputCodeElement.value
+    
+    //正規則表達式
+    const codePatternReg = /\w\w\w-\w\w\w-\w\w\w/g
+
+    if (inputCodeValue.length == 0 || codePatternReg.test(inputCodeValue) == false){
+        //console.log("沒有填資料")
+        codeAlertElement.classList.remove("close")
+        console.log("執行錯誤")
+        return
+    }
+    allOfDataArray.push({'code':inputCodeValue})
+}
+
+const checkRadioValue = () =>{
+    let radioElement = document.querySelectorAll('.form-check-label')
+    radioElements.forEach(element => {
+        
+    });
+}
+
+const clearAllAlertAndData = ()=>{
+    //清除產品警告
+    let nameAlertElement = document.querySelector('#nameAlert')
+    nameAlertElement.classList.add("close")
+
+    //清除code警告
+    let codeAlertElement = document.querySelector('#codeAlert')
+    codeAlertElement.classList.add("close")
+
+    //清除收集的資料
+    allOfDataArray = []
 }
 
 formElement.addEventListener('submit',(event)=>{
+    clearAllAlertAndData()
     event.preventDefault()
     validateName()
+    validateCodeFormat()
+    console.log(allOfDataArray)
 })
